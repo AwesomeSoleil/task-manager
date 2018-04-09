ea533bbfa7e93c208c133aa68195059479c67ec4
Dmitriy Startsev <startsevd@gmail.com>
2018-04-09 12:47:49 +0300

    pretty much functional; ugly, though

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,8 @@
 
 //  *************** form ***************
     const mount = document.querySelector('#mount');
+    const container = document.createElement('div');
+    container.setAttribute('id', 'container');
     const list = document.createElement('ul');
     const form = document.createElement('form');
     const input = document.createElement('input');
@@ -20,18 +22,15 @@
     calendar.setAttribute('min', `${new Date().toISOString().split('T')[0]}`);
     const submitButton = document.createElement('input');
     submitButton.setAttribute('type', 'submit');
-    submitButton.setAttribute('value', 'add new task');
-    const showCompleted = document.createElement('input');
-    showCompleted.setAttribute('type', 'checkbox');
-    showCompleted.setAttribute('name', 'completed');
-    showCompleted.addEventListener('click', filter);
+    submitButton.setAttribute('value', 'add new task'); 
     form.appendChild(input);
     form.appendChild(calendar);
     form.appendChild(submitButton);
-//    form.appendChild(showCompleted);
     form.addEventListener('submit', addTask);
-    mount.appendChild(form);
-    mount.appendChild(list);
+    container.appendChild(form);
+    createFilter();
+    container.appendChild(list);
+    mount.appendChild(container);
 
 //  *************** form ***************
 
@@ -53,10 +52,10 @@
         listItem.setAttribute('id', task.id);
         const subjectField = document.createElement('div');
         subjectField.className = 'task-subject';
-        subjectField.appendChild(document.createTextNode(`${task.subject}`));
+        subjectField.appendChild(document.createTextNode(task.subject));
         const dueDateField = document.createElement('div');
         dueDateField.className = 'task-deadline';
-        dueDateField.appendChild(document.createTextNode(`${task.dueDate}`));
+        dueDateField.appendChild(document.createTextNode(task.dueDate));
         const deleteButton = document.createElement('div');
         deleteButton.className = 'task-delete';        
         const removeTaskButton = document.createElement('button');
@@ -78,33 +77,70 @@
         list.appendChild(listItem);
     }
 
+    function createFilter() {
+        const values = ['all', 'completed', 'overdue'];
+        const fieldset = document.createElement('fieldset');
+        const legend = document.createElement('legend');
+        legend.appendChild(document.createTextNode('show: '));
+        fieldset.appendChild(legend);
+        values.forEach(function(value) {
+                const radioButton = document.createElement('input');
+                radioButton.setAttribute('type', 'radio');
+                radioButton.setAttribute('name', 'filter');
+                radioButton.setAttribute('value', value);
+                if (value === 'all') {
+                    radioButton.checked = true;
+                }
+                radioButton.addEventListener('click', filter);
+                const label = document.createElement('label');
+                label.appendChild(document.createTextNode(value));
+                label.appendChild(radioButton);
+                fieldset.appendChild(label);
+            }
+        );
+        container.appendChild(fieldset);
+    }
+
     function addTask(event) {
         event.preventDefault();
         tasks.push(new Task(input.value, calendar.value));
         localStorage.setItem('tasks', JSON.stringify(tasks));           
-        createListItem(input.value);
         form.reset();
         renderList(tasks);
     }
 
     function removeTask(event) {
-        const index = tasks.findIndex(function(task) { return task.id == event.path[1].id });
+        const index = tasks.findIndex(function(task) { return task.id == event.path[2].id });
         tasks.splice(index, 1);
         localStorage.setItem('tasks', JSON.stringify(tasks));
         renderList(tasks);
     }
 
     function toggleCompleted(event) {
-        const task = tasks.find(function(task) { return task.id == event.path[1].id });
+        const task = tasks.find(function(task) { return task.id == event.path[2].id });
         task.isCompleted = !task.isCompleted;
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
     function filter() {
         let filteredTasks;
-        if (event.target.name === 'completed') {
-        filteredTasks = tasks.filter(function(task) {return task.isCompleted === true});
-    }
+        switch(event.target.value) {
+            case 'all': filteredTasks = tasks;
+            break;
+            case 'completed': filteredTasks = tasks.filter(function(task) {
+                                                            return task.isCompleted === true;
+                                                        }
+                                                    );
+            break;
+            case 'overdue': filteredTasks = tasks.filter(function(task) {
+                                                        const rightNow = new Date();
+                                                        const deadline = new Date(task.dueDate);
+                                                        return rightNow > deadline;
+                                                    }
+                                                );
+            break;
+            default: filteredTasks = tasks;
+        }
         renderList(filteredTasks);
     }
 
